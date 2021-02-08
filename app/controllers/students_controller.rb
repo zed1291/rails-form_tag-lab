bd93f8fbf4830438922cd43a444128e863603c55
“zed” <zed@kolk.me>
2021-02-08 09:04:27 -0600

    think its done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    # Post.create(title: params[:post][:title], description: params[:post][:description])
+    # redirect_to posts_path
+    Students.create(name: params[:student][:name])
+  end
+
 end
